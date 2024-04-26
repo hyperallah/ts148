ea12f566dda8bb6cca0c9eeda0b7087acffbeae3
Mikhail Samoilov <johndoe@example.com>
2024-04-26 09:03:17 +0300

    serialize DateTime field to translated and readable format

--- app/Models/File.php
+++ app/Models/File.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -14,4 +15,9 @@ class File extends Model
         'extension',
         'name'
     ];
+
+    public function serializeTime(): string
+    {
+        return Carbon::instance($this->created_at)->translatedFormat('l, d F Y H:i');
+    }
 }
